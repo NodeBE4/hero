3988fa052f868ab90ecc9153e0beb8a00aa6a874
NodeBE4 <becare4@tutanota.com>
2020-09-02 06:34:19 +0000

    vote

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -50,3 +50,64 @@ function loadHeroes(url, id){
   })
 
 }
+
+function vote_hero(hash){
+  console.log(hash)
+  var btn = document.getElementById("vote-"+hash)
+  var cell = document.getElementById("vote-cnt-"+hash)
+  btn.parentNode.removeChild(btn)
+  var count = parseInt(cell.innerText) + 1
+  console.log(count)
+  cell.innerText = count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  var form = document.getElementById("vote-form")
+  var inputhash = document.getElementById("input-hash")
+  inputhash.value=hash
+  form.submit()
+  // let result = document.getElementById('bot-message'); 
+  // $('#vote-form').submit(function(){
+  //     $.ajax({
+  //       url: $('#form').attr('action'),
+  //       type: 'POST',
+  //       data : $('#form').serialize(),
+  //       success: function(response){
+  //         result.innerHTML  = `<p>${response}</p>` +result.innerHTML 
+  //         console.log('vote succeed');
+  //       }
+  //     });
+  //     return false;
+  // });
+  // let votedata = {
+  //   hash: hash
+  // }
+  // // Converting JSON data to string 
+  // var data = JSON.stringify(votedata); 
+  // let url = 'http://localhost:3000/api/vote'
+  // sendJSON(votedata, CORS_PROXY+url)
+  return false
+}
+
+function sendJSON(data, url){ 
+  let result = document.getElementById('bot-message'); 
+     
+  // Creating a XHR object 
+  let xhr = new XMLHttpRequest(); 
+
+  // open a connection 
+  xhr.open("POST", url, true); 
+
+  // Set the request header i.e. which type of content you are sending 
+  xhr.setRequestHeader("Content-Type", "application/json"); 
+
+  // Create a state change callback 
+  xhr.onreadystatechange = function () { 
+      if (xhr.readyState === 4 && xhr.status === 200) { 
+
+          // Print received data from server 
+          result.innerHTML = this.responseText; 
+
+      } 
+  }; 
+
+  // Sending data with the request 
+  xhr.send(data); 
+} 
