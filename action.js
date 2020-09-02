45426717806083b34f7fba660ecc30c7b8a916ec
NodeBE4 <becare4@tutanota.com>
2020-09-02 03:11:03 +0000

    submit hero

---
+++ action.js
@@ -0,0 +1,130 @@
+let { Octokit } = require('@octokit/rest')
+let { loadWikipedia, generateArticle } = require('./lib')
+const crypto = require('crypto');
+let fs = require('fs')
+
+require('dotenv').config()
+
+let TOKEN = process.env.TOKEN
+let REPOSITORY = process.env.REPOSITORY
+let [OWNER, REPO] = REPOSITORY.split('/')
+
+let octokit = new Octokit({
+  auth: TOKEN
+})
+
+const FILE = '100644'; // commit mode
+const encoding = 'utf-8';
+const ref = 'heads/master';
+
+async function performTasks() {
+  let { data } = await octokit.issues.listForRepo({
+    owner: OWNER,
+    repo: REPO,
+    state: 'open'
+  })
+
+  let promises = data.map(async (issue) => {
+    try {
+      if (issue.title.substring(0,12)=='add_request:'){
+        let titletext = issue.title.split(" ")[1]
+        let people = titletext.split("-")[0].trim()
+        let keyword = titletext.replace(people,'').replace("-",'')
+        let wiki = issue.body.split('\n')[0]
+        let match = /^https:\/\/(zh|en).wikipedia.org\/wiki\//
+        if (match.test(wiki)){
+            let rawfile = await octokit.repos.getContent({
+                            owner: OWNER,
+                            repo: REPO,
+                            path: `index.json`,
+                            ref: ref
+                        })
+            let buff = new Buffer.from(rawfile.data.content, 'base64')
+            let text = buff.toString('utf-8')
+            let json = JSON.parse(text)
+            let hash = crypto.createHash('md5').update(people+wiki).digest("hex")
+            var thisperson = json.filter(function (item) {
+                return item.wiki == wiki ;
+            }); 
+            if (thisperson.length > 0){
+              await octokit.issues.createComment({
+                owner: OWNER,
+                repo: REPO,
+                issue_number: issue.number,
+                body: `${thisperson[0].people} 已经榜上有名了，试试到网页上的search他/她的名字`
+              })
+              await octokit.issues.update({
+                owner: OWNER,
+                repo: REPO,
+                issue_number: issue.number,
+                state: 'closed',
+                title: articleData.title,
+                labels: ['duplicated']
+              })
+            }else{
+              let newhero = {
+                people: people,
+                keyword: keyword,
+                wiki: wiki,
+                vote: 1,
+                hash: hash
+              }
+              json.push({
+                ...newhero
+              });
+              console.log(json);
+              let content = JSON.stringify(json, undefined, 4);
+
+              let committer = {
+              name: 'NodeBE4',
+              email: 'you@example.com'
+              }
+
+              let prTitle = `添加新人物-${people}-${wiki}`
+
+              let intro = await loadWikipedia(newhero.wiki, "mw-content-text")
+              let page = generateArticle(newhero, intro)
+
+              fs.writeFileSync(`./index.json`, content)
+
+              await octokit.issues.createComment({
+                owner: OWNER,
+                repo: REPO,
+                issue_number: issue.number,
+                body: page
+              })
+              await octokit.issues.update({
+                owner: OWNER,
+                repo: REPO,
+                issue_number: issue.number,
+                state: 'closed',
+                title: prTitle,
+                labels: ['success']
+              })
+            }
+           }
+          }
+        } catch(error) {
+          await octokit.issues.createComment({
+            owner: OWNER,
+            repo: REPO,
+            issue_number: issue.number,
+            body: `错误 ${error.toString()}`
+          })
+          await octokit.issues.update({
+            owner: OWNER,
+            repo: REPO,
+            issue_number: issue.number,
+            state: 'closed',
+            labels: ['error']
+          })
+          throw error
+        }
+
+  })
+
+  await Promise.all(promises)
+}
+
+
+performTasks()
--- perform.js
+++ perform.js
@@ -6,7 +6,7 @@ const https = require('https')
 const fetch = require('node-fetch')
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-let document = require('html-element').document
+let { loadRefSites, loadWikipedia, generateArticle } = require('./lib')
 let URL = urlMod.URL
 
 let db_news_url = 'https://nodebe4.github.io/waimei/search.json'
@@ -15,97 +15,11 @@ const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 
 let settings = { method: "Get" }
 
-let news = []
-let oped = []
-
-function getElementById(node, id) {    
-    return node.querySelector("#" + id);
-}
-
-async function loadWikipedia(url, id){
-  // var div = document.getElementById('submitText');
-  // const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-  // var html = document.createElement("div");
-  // html.id = "about";
-
-  const response = await fetch(url, settings);
-  const body = await response.text();
-
-  const dom = new JSDOM(body)
-  let content = dom.window.document.querySelector("#"+id);
-  var tables = content.querySelector("table");
-  tables.parentNode.removeChild(tables);
-  // for (i=0;i<tables.length;i++){
-  //   tables[i].parentNode.removeChild(tables[i]);
-  //   // tables[i].remove();
-  // }
-  var toc = content.querySelector("#toc");
-  if (toc == null ){
-    var toc = content.querySelector("h2");
-  }
-  if (toc){
-    while(toc.nextSibling){
-      var element = toc.nextSibling;
-      element.parentNode.removeChild(element);
-    }
-    toc.parentNode.removeChild(toc);
-  }
-  // el.innerHTML = html.innerHTML;
-  // html.appencChild(content)
-
-  return content.innerHTML;
-
-}
-
-function loadNews(hero){
-  const html = (new JSDOM(`<div><h3></h3><ul></ul></div>`)).window.document.querySelector("div")
-  const ul = html.querySelector("ul")
-  const h3 = html.querySelector("h3")
-  html.id = "recent-news"
-  h3.innerHTML = "最近动态"
-  let relatednews = news.map(item => {
-    if (item['title'].includes(hero) || item['desc'].includes(hero)){
-      var baseurl = "https://nodebe4.github.io"
-      ul.innerHTML += `<li><a href="${baseurl+item['url']}" title="${item['desc']}">${item['title']}</a><time>${item['date']}</time><a class="tag">${item['category']}</a></li>\n`
-      return item
-    }
-  })
-  return html.parentElement.innerHTML
-}
-
-function loadOped(hero){
-  const html = (new JSDOM(`<div><h3></h3><ul></ul></div>`)).window.document.querySelector("div")
-  const ul = html.querySelector("ul")
-  const h3 = html.querySelector("h3")
-  html.id = "open-opinion"
-  h3.innerHTML = "过往言论"
-  let relatednews = oped.map(item => {
-    if (item['title'].includes(hero) || item['desc'].includes(hero)){
-      var baseurl = "https://nodebe4.github.io"
-      ul.innerHTML += `<li><a href="${baseurl+item['url']}" title="${item['desc']}">${item['title']}</a><time>${item['date']}</time><a class="tag">${item['category']}</a></li>\n`
-      return item
-    }
-  })
-  return html.parentElement.innerHTML
-}
-
 async function perform() {
   let rawdata = fs.readFileSync('./index.json', {encoding:'utf8', flag:'r'})
   let heroes = JSON.parse(rawdata)
   // console.log(heroes)
-
-  await fetch(db_news_url, settings)
-    .then(res => res.json())
-    .then((json) => {
-      news = json
-    });
-
-  await fetch(db_oped_url, settings)
-    .then(res => res.json())
-    .then((json) => {
-      oped = json
-    });
-
+  await loadRefSites()
 
   await Promise.all(heroes.map(async (item) => {
     let intro = await loadWikipedia(item.wiki, "mw-content-text")
@@ -113,38 +27,10 @@ async function perform() {
   }));
 }
 
-function generateArticle(item, intro) {
-  let today = new Date()
-  let openoped = loadOped(item['people'])
-  let recentnews = loadNews(item['people'])
-  let md = intro + recentnews + openoped
-  let dateString = "1989-06-04"
-  let titletext = item.people.toString().replace(/"/g, '\\"').replace("...", '')
-  let articlelink = new URL(item.wiki).href
-  let header = `---
-layout: post
-title: "${titletext}"
-date: ${dateString}
-author: 维基百科
-from: ${articlelink}
-tags: [ ${titletext}, 维基百科 ]
-categories: [ ${titletext} ]
----
-`
-  md = header + md
-  let filename = `${dateString.substring(0, 10)}-${titletext.substring(0, 50)}.md`.replace(/\//g, '--')
-  fs.writeFileSync(`./_posts/${filename}`, md)
-  console.log(`add ./_posts/${filename}`)
-  // if (!fs.existsSync(`./_posts/${filename}`)) {
-  //   fs.writeFileSync(`./_posts/${filename}`, md)
-  //   console.log(`add ./_posts/${filename}`)
-  // }
-}
-
-
 function timeConverter(UNIX_timestamp){
   var a = new Date(UNIX_timestamp);
   return a
 }
 
 perform()
+
